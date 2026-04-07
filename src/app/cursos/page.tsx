b0fd9472c2458cd3cce005208d45e3595f76ca67
pslcorrea <pslcorrea@gmail.com>
2026-04-07 16:41:32 -0300

    feat: criação header component

---
+++ src/app/cursos/page.tsx
@@ -0,0 +1,7 @@
+export default function PageCursos() {
+  return (
+    <main>
+      <h1>Cursos</h1>
+    </main>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,23 @@
-import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Nunito } from 'next/font/google';
+
+import './globals.css';
+import { Header } from '../components/header/Header';
 
 const geistSans = Nunito({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Nunito({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Codarse - Cursos de Programação Gratuitos",
+  title: 'Codarse - Cursos de Programação Gratuitos',
   description:
-    "Os melhores cursos de programação gratuitos com a melhor experiência de aprendizado e foco",
+    'Os melhores cursos de programação gratuitos com a melhor experiência de aprendizado e foco',
 };
 
 export default function RootLayout({
@@ -28,7 +30,10 @@ export default function RootLayout({
       lang="pt-Br"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
