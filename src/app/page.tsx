ce8dcfb8b92ceedbb3652f2d53a79069a845ca2b
pslcorrea <pslcorrea@gmail.com>
2026-04-07 14:11:54 -0300

    feat: Configuração de tema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function PageHome() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1 className="bg-primary-contrast">Home</h1>
     </main>
   );
 }
