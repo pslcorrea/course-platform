40748161df157fd15e7a4530a40b425951e9d87d
pslcorrea <pslcorrea@gmail.com>
2026-04-08 17:10:21 -0300

    feat: add section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import { Card } from "../components/card/Card";
+import { Section } from "../components/section/Section";
 
 export const metadata: Metadata = {
   title: "CodarSe - Página Inicial",
@@ -7,12 +7,7 @@ export const metadata: Metadata = {
 export default function PageHome() {
   return (
     <main>
-      <Card
-        href="/cursos/123"
-        image="https://i.ytimg.com/vi/SVepTuBK4V0/default.jpg"
-        title="Curso de API Rest, Node e Typescript"
-        description={`Curso de API Rest em Node e Typescript vamos ver juntos o escopo do curso e se esse curso faz sentido para você ou não. Quero te apresentar as tecnologias e conceitos que vamos usar no projeto que será desenvolvido ao longo do curso, ainda vamos entender como serão  criados os testes que usaremos para garantir a qualidade do código e no final vamos ver o frontend que eu vou te entregar de brinde para usar com esse backend.`}
-      />
+      <Section />
     </main>
   );
 }
