ae7c265764f19fc303b3f18873d85a9bf895eb25
pslcorrea <pslcorrea@gmail.com>
2026-04-08 12:07:08 -0300

    feat: add navigation drawer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image';
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "CodarSe - Página Inicial",
+};
 export default function PageHome() {
   return (
     <main>
-      <h1 className="bg-primary-contrast">Home</h1>
+      <h1>Home</h1>
     </main>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,25 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 import { MdMenu, MdOutlineOpenInNew } from "react-icons/md";
 
 export const Header = () => {
+  const [title, setTitle] = useState("CodarSe");
+  const [drawer, setDrawer] = useState(false);
   const currentPath = usePathname();
+
+  useEffect(() => {
+    setTitle(document.title);
+    setDrawer(false);
+  }, [currentPath]);
 
   return (
     <nav className="flex gap-6 items-center justify-start md:justify-center bg-primary py-2 sm:py-4 px-6">
-      <button className="sm:hidden">
+      <button className="sm:hidden" onClick={() => setDrawer(true)}>
         <MdMenu size={24} />
       </button>
-      <ul className="flex gap-4 items-center">
+      <ul className="flex gap-4 items-center" tabIndex={drawer ? -1 : undefined}>
         <li className="my-2">
           <Link href="/" className="border-2 rounded-md py-2 px-1 font-bold">
             CODARSE
@@ -46,7 +54,50 @@ export const Header = () => {
           </Link>
         </li>
       </ul>
-      <h1 className="sm:hidden">CodarSe - Página inicial</h1>
+
+      {/* Drawer */}
+      <div
+        data-open={drawer}
+        tabIndex={drawer ? undefined : -1}
+        onClick={() => setDrawer(false)}
+        className="bg-gradient-to-r from-background fixed top-0 left-0 bottom-0 right-0 transition-transform data-[open=false]:-translate-x-full"
+      >
+        <ul
+          className="flex gap-4 flex-col p-4 w-60 h-full bg-background"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <li className="hidden sm:block">
+            <Link
+              href="/"
+              data-active={currentPath === "/"}
+              className="data-[active=true]:underline"
+            >
+              Página Inicial
+            </Link>
+          </li>
+          <li className="hidden sm:block">
+            <Link
+              href="/cursos"
+              data-active={currentPath === "/cursos"}
+              className="data-[active=true]:underline"
+            >
+              Cursos
+            </Link>
+          </li>
+          <li className="hidden sm:block">
+            <Link
+              href="https://codarse.com"
+              target="_blank"
+              className="flex items-center gap-1"
+            >
+              Blog
+              <MdOutlineOpenInNew />
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <h1 className="sm:hidden">{title}</h1>
     </nav>
   );
 };
